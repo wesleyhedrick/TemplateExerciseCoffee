161d0fba40de20036250659d752457803cd6a04a
Chris Aquino <chris@digitalcrafts.com>
2020-12-10 17:08:53 -0500

    adds final route + controller + view

--- controllers.js
+++ controllers.js
@@ -77,7 +77,45 @@ const orderDetails = (req, res) => {
     // particular kind. (I think lol)
 
     // and how do I even get to this route?
-    res.send('does this fn work at all?');
+    //res.send('does this fn work at all?');
+
+    // after creating the href in the list view
+    // I'm going to send back the 
+    // results of models.ordersByType()
+    const orderType = req.params.orderType;
+    // res.json({
+    //     orderType,
+    //     // orders is an array of objects...
+    //     // but I only need the cost...
+    //     orders: models.ordersByType(orderType) // need an argument!
+    //                   .map(o => o.cost) // pluck out the .cost
+    // });
+    // Now to create the template
+    // It'll be similar to my list.html
+    // We want:
+    /*
+    <h3>espresso</h3>
+    <ul>
+        <li>$2.44</li>
+        <li>$1.46</li>
+        ...
+    </ul>
+    */
+   // haha I need to res.render()
+   res.render('details', {
+       locals: {
+            orderType,
+            // orders is an array of objects...
+            // but I only need the cost...
+            orders: models.ordersByType(orderType) // need an argument!
+                        .map(o => o.cost) // pluck out the .cost
+        },
+        partials: {
+            header: '/partials/header',
+            footer: '/partials/footer',
+        }
+   })
+
 };
 
 
